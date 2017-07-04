8dda4cee428f1db87de971ec29adbbb79bebe576
Richard Bramley <rabramley@gmail.com>
2017-07-04 10:03:24 +0100

    Sue wants it too

--- reporter/reporter/reports/bioresource/data_quality/pmi_patient_mismatch.py
+++ reporter/reporter/reports/bioresource/data_quality/pmi_patient_mismatch.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
 from reporter.reports import PmiPatientMismatch
-from reporter import RECIPIENT_BIORESOURCE_ADMIN
+from reporter import RECIPIENT_BIORESOURCE_ADMIN, RECIPIENT_IT_DQ
 
 
 class BioresourcePmiPatientMismatch(PmiPatientMismatch):
     def __init__(self):
         super().__init__(
             project='i2b2_app03_bioresource_Data',
-            recipients=[RECIPIENT_BIORESOURCE_ADMIN]
+            recipients=[RECIPIENT_BIORESOURCE_ADMIN, RECIPIENT_IT_DQ]
         )
