7396d3df2b2e44fb4f20aaff769ba0ea50d33c90
Richard Bramley <rabramley@gmail.com>
2017-07-10 16:57:05 +0100

    Missing check dates

---
+++ reporter/reporter/reports/bioresource/data_quality/without_check_study_consent.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+from reporter.reports import Report
+from reporter import (
+    get_case_link, get_contact_id_search_link,
+    RECIPIENT_BIORESOURCE_ADMIN)
+
+
+class BioresourceWithoutCheckStudyConsent(Report):
+    def __init__(self):
+        super().__init__(
+            introduction=("The following participants do not  "
+                          "have a competed Check Study Consent "
+                          "activity in CiviCRM"),
+            recipients=[RECIPIENT_BIORESOURCE_ADMIN],
+            sql='''
+
+SELECT
+    rc.bioresource_id,
+    CONVERT(DATE, REPLACE(rc.consent_date, '-', ''), 112) consent_date,
+    cv.civicrm_case_id,
+    cv.civicrm_contact_id
+FROM [i2b2_app03_bioresource_Data].[dbo].[LOAD_Redcap_Bioresource] rc
+LEFT JOIN [i2b2_app03_bioresource_Data].[dbo].[LOAD_Civicrm_Bioresource] cv
+    ON (cv.bioresource_id = rc.bioresource_id
+        OR cv.legacy_bioresource_id = rc.bioresource_id)
+WHERE
+    COALESCE(
+        rc.consent_1,
+        rc.consent_2,
+        rc.consent_3,
+        rc.consent_4,
+        rc.consent_5,
+        rc.consent_6) IS NOT NULL
+    AND NOT EXISTS (
+        SELECT 1
+        FROM STG_CiviCRM.dbo.civicrm_case cas
+        JOIN STG_CiviCRM.dbo.civicrm_value_nihr_bioresource_11 bio
+            ON bio.entity_id = cas.id
+        JOIN STG_CiviCRM.dbo.civicrm_case_activity cas_act
+            ON cas_act.case_id = cas.id
+        JOIN    STG_CiviCRM.dbo.civicrm_activity act
+            ON act.id = cas_act.activity_id
+            AND act.activity_type_id = 56
+            AND act.is_deleted = 0
+            AND act.is_current_revision = 1
+            AND act.status_id = 2
+        WHERE cas.case_type_id = 7
+            AND cas.is_deleted = 0
+            AND (
+                    bio.nihr_bioresource_id_41 = rc.bioresource_id
+                OR bio.nihr_bioresource_legacy_id_78 = rc.bioresource_id
+            )
+    )
+
+                '''
+        )
+
+    def get_report_line(self, row):
+        if row['civicrm_case_id'] and row['civicrm_contact_id']:
+            link = get_case_link(
+                row['bioresource_id'],
+                row["civicrm_case_id"],
+                row["civicrm_contact_id"])
+        else:
+            link = get_contact_id_search_link(
+                row["bioresource_id"],
+                row["bioresource_id"])
+
+        return '- {} Consent Date: {}\r\n'.format(
+            link, row['consent_date'] or '')
+
+
+r = BioresourceWithoutCheckStudyConsent()
+r. run()
