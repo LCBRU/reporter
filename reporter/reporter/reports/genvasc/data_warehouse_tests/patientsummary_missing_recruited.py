926ed9e60ed3fca4377a558475df90fb610fbc59
Richard Bramley <rabramley@gmail.com>
2017-09-12 10:59:09 +0100

    GENVASC patient summary check

---
+++ reporter/reporter/reports/genvasc/data_warehouse_tests/patientsummary_missing_recruited.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from reporter.reports import Report
+from reporter import get_case_link, RECIPIENT_IT_DWH
+
+
+class GenvascPatientSummaryMissingRecruited(Report):
+    def __init__(self):
+        super().__init__(
+            introduction=("The following participants have an error "
+                          "so they have not reached i2b2"),
+            recipients=[RECIPIENT_IT_DWH],
+            sql='''
+
+SELECT
+    a.CaseId,
+    a.CiviCrmId
+FROM i2b2_app03_genvasc_Data.[dbo].LOAD_ValidEnrollments a
+WHERE NOT EXISTS (
+        SELECT 1
+        FROM i2b2_app03_genvasc_Data.[dbo].PatientSummary
+        WHERE StudyNumber = a.StudyNumber
+    )
+
+                '''
+        )
+
+    def get_report_line(self, row):
+        return '- {}\r\n\r\n'.format(
+            get_case_link(
+                'Click to View',
+                row["CaseId"],
+                row["CiviCrmId"]))
