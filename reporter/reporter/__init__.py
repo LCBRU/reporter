32fe8c7482555a033b26752663188d5d9d62440a
Richard Bramley <rabramley@gmail.com>
2017-09-18 15:27:18 +0100

    Fast civi / redcap mismatches

--- reporter/reporter/__init__.py
+++ reporter/reporter/__init__.py
@@ -170,3 +170,31 @@ def get_contact_id_search_link(link_text, contact_id):
     return (CIVICRM_SEARCH_URL.format(
         link_text,
         contact_id))
+
+
+def get_redcap_link(link_text, project_id, record):
+    REDCAP_VERSION = 'v7.2.2'
+    REDCAP_RECORD_URL = (
+        '[{}](https://briccs.xuhl-tr.nhs.uk/redcap/'
+        'redcap_{}/DataEntry/record_home.php'
+        '?pid={}&id={})')
+
+    return (REDCAP_RECORD_URL.format(
+        link_text,
+        REDCAP_VERSION,
+        project_id,
+        record))
+
+
+def get_redcap_external_link(link_text, project_id, record):
+    REDCAP_VERSION = 'v7.2.2'
+    REDCAP_RECORD_URL = (
+        '[{}](https://uhlbriccsext01.xuhl-tr.nhs.uk/redcap/'
+        'redcap_{}/DataEntry/record_home.php'
+        '?pid={}&id={})')
+
+    return (REDCAP_RECORD_URL.format(
+        link_text,
+        REDCAP_VERSION,
+        project_id,
+        record))
---
+++ reporter/reporter/reports/fast/data_quality/Redcap_Not_Civi.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+from reporter.reports import Report, Schedule
+from reporter import (RECIPIENT_FAST_MANAGER, RECIPIENT_FAST_ADMIN,
+                      get_redcap_link)
+
+
+class FastRedcapNotCivicrmReport(Report):
+    def __init__(self):
+        super().__init__(
+            introduction=("The following FAST participants are "
+                          "recruited in REDCap, but are not "
+                          "recruited is CiviCRM:"),
+            recipients=[RECIPIENT_FAST_MANAGER, RECIPIENT_FAST_ADMIN],
+            schedule=Schedule.weekly,
+            sql='''
+SELECT
+    ri.value AS fast_id,
+    ri.project_id AS project_id,
+    ri.record
+FROM    STG_redcap.dbo.redcap_data ri
+JOIN    STG_redcap.dbo.redcap_data pr
+    ON pr.record = ri.record
+    AND pr.project_id = ri.project_id
+    AND pr.field_name = 'patient_reruited'
+WHERE   ri.project_id = 48
+    AND ri.field_name = 'record_id'
+    AND pr.value = '1'
+    AND NOT EXISTS (
+        SELECT  1
+        FROM    STG_CiviCRM.dbo.civicrm_case cas
+        JOIN    STG_CiviCRM.dbo.civicrm_case_contact cas_con
+            ON cas_con.case_id = cas.id
+        JOIN    STG_CiviCRM.dbo.civicrm_contact con
+            ON con.id = cas_con.contact_id
+            AND con.is_deleted = 0
+        JOIN    STG_CiviCRM.dbo.civicrm_value_fast_24 fst
+            ON fst.entity_id = cas.id
+        WHERE cas.case_type_id = 18 -- FAST
+            AND cas.is_deleted = 0
+            AND cas.status_id = 5 -- Recruited
+            AND fst.fast_id_106 = ri.value
+    )
+
+                '''
+        )
+
+    def get_report_line(self, row):
+        return '- {}\r\n\r\n'.format(
+            get_redcap_link(
+                row['fast_id'],
+                row["project_id"],
+                row["record"]))
