ac491eb9e3b32e7b88e76235dd7fdbfe397e4353
Richard Bramley <rabramley@gmail.com>
2017-12-05 15:09:44 +0000

    Add TMAO duplicate study number report

--- reporter/reporter/reports/tmao/data_quality/civicrm.py
+++ reporter/reporter/reports/tmao/data_quality/civicrm.py
@@ -3,7 +3,8 @@
 from reporter.reports import Report, Schedule
 from reporter import (
     RECIPIENT_TMAO_ADMIN,
-    get_case_link
+    get_case_link,
+    get_contact_id_search_link
 )
 
 
@@ -44,3 +45,48 @@ WHERE cas.is_deleted = 0
                 'Click Here',
                 row['case_id'],
                 row['contact_id']))
+
+
+class TmaoCiviCrmDuplicateStudyNumber(Report):
+    def __init__(self):
+        super().__init__(
+            introduction=("The following TMAO enrolments have "
+                          "duplicated study numbers in CiviCRM"),
+            recipients=[RECIPIENT_TMAO_ADMIN],
+            sql='''
+
+SELECT
+    std.StudyNumber
+FROM    STG_CiviCRM.dbo.civicrm_case cas
+JOIN    STG_CiviCRM.dbo.civicrm_case_contact cas_con
+    ON cas_con.case_id = cas.id
+JOIN    STG_CiviCRM.dbo.civicrm_contact con
+    ON con.id = cas_con.contact_id
+    AND con.is_deleted = 0
+JOIN (
+    SELECT
+        s.entity_id AS case_id,
+        12 AS case_type_id,
+        s.tmao_id_79 AS StudyNumber
+    FROM STG_CiviCRM.dbo.civicrm_value_tmao_18 s
+) std ON std.case_id = cas.id
+    AND std.case_type_id = cas.case_type_id
+WHERE cas.is_deleted = 0
+    AND cas.status_id IN (
+        5, --recruited
+        8  -- withdrawn
+    )
+    AND i2b2ClinDataIntegration.dbo.IsNullOrEmpty(std.StudyNumber) = 0
+GROUP BY std.StudyNumber
+HAVING COUNT(*) > 1
+;
+
+                ''',
+                schedule=Schedule.daily
+        )
+
+    def get_report_line(self, row):
+        return '- {}\r\n'.format(
+            get_contact_id_search_link(
+                row['StudyNumber'],
+                row['StudyNumber']))
