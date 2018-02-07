152ab7faf95b3e2785302c0ed68bfaa95b2a6e29
Richard Bramley <rabramley@gmail.com>
2018-02-07 12:01:20 +0000

    Invalid post code report

---
+++ reporter/reporter/reports/civicrm/invalid_post_code.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from reporter.reports import SqlReport, Schedule
+from reporter.reports.civicrm import get_contact_link
+from reporter.reports.emailing import RECIPIENT_IT_DQ
+
+
+class CivicrmInvalidPostCode(SqlReport):
+    def __init__(self):
+        super().__init__(
+            introduction=("The following contacts have an address "
+                          "with an invalid post code"),
+            recipients=[RECIPIENT_IT_DQ],
+            sql='''
+SELECT
+    con.id AS contact_id,
+    a.postal_code
+FROM STG_CiviCRM.dbo.civicrm_address a
+JOIN STG_CiviCRM.dbo.civicrm_contact con
+    ON con.id = a.contact_id
+WHERE i2b2ClinDataIntegration.dbo.isInvalidPostcode(postal_code) = 1
+                ''',
+                schedule=Schedule.daily
+        )
+
+    def get_report_line(self, row):
+        return '- {}\r\n'.format(
+            get_contact_link(row['postal_code'], row['contact_id'])
+        )
