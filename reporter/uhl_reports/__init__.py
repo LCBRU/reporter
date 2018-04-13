8bef972fd7ef6d857bca9c44cf93a32c412508c9
Richard Bramley <rabramley@gmail.com>
2018-04-13 13:52:23 +0100

    Just get the sub modules of this path

--- reporter/uhl_reports/__init__.py
+++ reporter/uhl_reports/__init__.py
@@ -1,17 +1,20 @@
-import importlib as _importlib
-import pkgutil as _pkgutil
+import importlib
+import pkgutil
 
-for m in _pkgutil.walk_packages("reporter." + __name__):
-    _importlib.import_module(m[1])
 
-'''
-__sub_mods__ = [".".join(_mod[1].split(".")[1:]) for _mod in
-                filter(
-                    lambda _mod: _mod[1].count(".") > 1 and not
-                    _mod[2] and __name__ in _mod[1],
-                    [_mod for _mod in
-                        _pkgutil.walk_packages("." + __name__)])]
+def get_sub_modules(path, prefix):
+    result = []
 
-for _module in __sub_mods__:
-    _importlib.import_module("." + _module, package='reporter')
-'''
+    for m in pkgutil.iter_modules(path):
+        new_module_name = prefix + m[1]
+        result.append(new_module_name)
+        result.extend(get_sub_modules(
+            [path[0] + '/' + m[1]],
+            new_module_name + '.'
+        ))
+
+    return result
+
+
+for m in get_sub_modules(__path__, __name__ + '.'):
+    importlib.import_module(m)
