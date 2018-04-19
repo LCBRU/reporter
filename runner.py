17ae85e917a7224eb6790b9182093dc34cf4d16c
Richard Bramley <rabramley@gmail.com>
2018-04-19 12:39:56 +0100

    Refactor to centralise running and scheduling of reports

---
+++ runner.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import logging
+import argparse
+from reporter.core import run_all, schedule_reports, run_reports
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+def get_parameters():
+    parser = argparse.ArgumentParser(description='Run specific reports.')
+    parser.add_argument(
+        'report_names',
+        metavar='report_names',
+        nargs='*',
+        help='Report names or start of the report name',
+    )
+    parser.add_argument(
+        '-x',
+        '--exclude',
+        nargs='*',
+        help='Reports names to exclude',
+        default=[]
+    )
+    parser.add_argument(
+        "-a",
+        "--all",
+        help="Run all reports",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+def run():
+    args = get_parameters()
+
+    exclude = [x.lower() for x in args.exclude]
+
+    if args.all:
+        run_all(exclude)
+
+        logging.info("---- All reports run ----")
+    elif not args.report_names:
+        schedule_reports()
+    else:
+        for report_name in args.report_names:
+            run_reports(report_name, exclude)
+
+        logging.info("---- All reports run ----")
