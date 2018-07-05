d54844d9eaab1dcae622fc2e94b6f1ef8f5a7b64
Richard Bramley <rabramley@gmail.com>
2018-07-05 15:18:29 +0100

    ANSI standard date validation

--- reporter/uol_redcap_reports/cto/data_quality.py
+++ reporter/uol_redcap_reports/cto/data_quality.py
@@ -1,28 +1,13 @@
 #!/usr/bin/env python3
 
 import re
-from reporter.core import SqlReport
 from reporter.connections import RedcapInstance
-from reporter.connections import DatabaseConnection
 from reporter.emailing import (
-    RECIPIENT_BRAVE_ADMIN as RECIPIENT_ADMIN,
-    RECIPIENT_BRAVE_MANAGER as RECIPIENT_MANAGER,
+    RECIPIENT_FOAMI_ADMIN as RECIPIENT_ADMIN,
+    RECIPIENT_FOAMI_MANAGER as RECIPIENT_MANAGER,
 )
 from reporter.application_abstract_reports.redcap.data_quality import (
     RedcapInvalidNhsNumber,
 )
 
-
 REDCAP_PROJECT_ID = 25
-
-# Abstract Reports
-
-
-class FoamiRedcapInvalidNhsNumber(RedcapInvalidNhsNumber):
-    def __init__(self):
-        super().__init__(
-            redcap_instance=RedcapInstance.uol_lamp,
-            project_id=REDCAP_PROJECT_ID,
-            fields=['nhs_num'],
-            recipients=[RECIPIENT_ADMIN, RECIPIENT_MANAGER],
-        )
