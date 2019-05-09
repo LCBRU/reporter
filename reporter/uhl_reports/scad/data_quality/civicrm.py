bc5518ea203483d34d462a197f0060d496982284
Richard Bramley <rabramley@gmail.com>
2019-05-09 17:41:10 +0100

    Civi SCAD reg ID should exist

--- reporter/uhl_reports/scad/data_quality/civicrm.py
+++ reporter/uhl_reports/scad/data_quality/civicrm.py
@@ -49,42 +49,38 @@ class ScadCivicrmInvalidCaseStatus(CivicrmInvalidCaseStatus):
             [RECIPIENT_SCAD_ADMIN])
 
 
-###
-### Reg ID Column not yet available
-###
-#  
-# class ScadCivicrmMissingRegId(SqlReport):
-#     def __init__(self):
-#         super().__init__(
-#             introduction=("The following enrolments do not have "
-#                           "SCAD Registry IDs in CiviCRM"),
-#             recipients= [RECIPIENT_SCAD_ADMIN],
-#             schedule=Schedule.daily,
-#             sql='''
+class ScadCivicrmMissingRegId(SqlReport):
+    def __init__(self):
+        super().__init__(
+            introduction=("The following enrolments do not have "
+                          "SCAD Registry IDs in CiviCRM"),
+            recipients= [RECIPIENT_SCAD_ADMIN],
+            schedule=Schedule.daily,
+            sql='''
 
-# SELECT
-#     cd.case_type_id,
-#     cd.civicrm_contact_id,
-#     cd.civicrm_case_id,
-#     cd.case_type_name
-# FROM STG_CiviCRM.dbo.LCBRU_CaseDetails cd
-# WHERE cd.case_status_id IN (
-#         5, --recruited
-#         8,  -- withdrawn
-#         6 -- Available for Cohort
-#     )
-#     AND i2b2ClinDataIntegration.dbo.IsNullOrEmpty(cd.StudyNumber) = 1
-#     AND cd.case_type_id = %s
+SELECT
+    cd.case_type_id,
+    cd.civicrm_contact_id,
+    cd.civicrm_case_id,
+    cd.case_type_name
+FROM STG_CiviCRM.dbo.LCBRU_CaseDetails cd
+WHERE cd.case_status_id IN (
+        5, --recruited
+        8,  -- withdrawn
+        6 -- Available for Cohort
+    )
+    AND i2b2ClinDataIntegration.dbo.IsNullOrEmpty(cd.StudyNumber2) = 1
+    AND cd.case_type_id = %s
 
-#                 ''',
-#             parameters=(CASE_TYPE_ID)
-#         )
+                ''',
+            parameters=(CASE_TYPE_ID)
+        )
 
-#     def get_report_line(self, row):
-#         return '- {}\r\n'.format(
-#             get_case_link(
-#                 row['case_type_name'],
-#                 row['civicrm_case_id'],
-#                 row['civicrm_contact_id']))
+    def get_report_line(self, row):
+        return '- {}\r\n'.format(
+            get_case_link(
+                row['case_type_name'],
+                row['civicrm_case_id'],
+                row['civicrm_contact_id']))
 
 
