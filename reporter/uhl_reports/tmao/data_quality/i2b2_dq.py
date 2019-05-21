f3266aff0cbb20a8356ec2491a26c629da533e9b
Richard Bramley <rabramley@gmail.com>
2019-05-21 14:17:37 +0100

    Archive TMAO

--- reporter/uhl_reports/tmao/data_quality/i2b2_dq.py
+++ reporter/uhl_reports/tmao/data_quality/i2b2_dq.py
@@ -16,6 +16,7 @@ from reporter.uhl_reports.i2b2.valid_enrolment_tests import (
 from reporter.emailing import (
     RECIPIENT_TMAO_ADMIN as RECIPIENT_ADMIN,
 )
+from reporter.core import Schedule
 
 
 I2B2_DB = "i2b2_app03_tmao_Data"
@@ -24,19 +25,28 @@ I2B2_DB = "i2b2_app03_tmao_Data"
 class TmaoPatientMappingDuplicatesReport(
         PatientMappingDuplicatesReport):
     def __init__(self):
-        super().__init__(I2B2_DB)
+        super().__init__(
+            I2B2_DB,
+            schedule=Schedule.never,
+        )
 
 
 class TmaoPatientMappingMultiplesIdsReport(
         PatientMappingMultiplesIdsReport):
     def __init__(self):
-        super().__init__(I2B2_DB)
+        super().__init__(
+            I2B2_DB,
+            schedule=Schedule.never,
+        )
 
 
 class TmaoatientSummaryDuplicatesReport(
         PatientSummaryDuplicatesReport):
     def __init__(self):
-        super().__init__(I2B2_DB)
+        super().__init__(
+            I2B2_DB,
+            schedule=Schedule.never,
+        )
 
 
 class TmaoPatientSummaryMissingData(
@@ -51,14 +61,18 @@ class TmaoPatientSummaryMissingData(
                 'StudyNumber',
                 'Gender',
                 'DateOfBirth',
-            ]
+            ],
+            schedule=Schedule.never,
         )
 
 
 class TmaoPatientSummaryMissingParticipants(
         PatientSummaryMissingParticipants):
     def __init__(self):
-        super().__init__(I2B2_DB)
+        super().__init__(
+            I2B2_DB,
+            schedule=Schedule.never,
+        )
 
 
 class TmaoValidEnrolmentsStudyIdDuplicates(
@@ -66,7 +80,8 @@ class TmaoValidEnrolmentsStudyIdDuplicates(
     def __init__(self):
         super().__init__(
             I2B2_DB,
-            [RECIPIENT_ADMIN]
+            [RECIPIENT_ADMIN],
+            schedule=Schedule.never,
         )
 
 
@@ -75,5 +90,6 @@ class TmaoValidEnrolmentsContactMultipleRecruitments(
     def __init__(self):
         super().__init__(
             I2B2_DB,
-            [RECIPIENT_ADMIN]
+            [RECIPIENT_ADMIN],
+            schedule=Schedule.never,
         )
