46340a6ef1544c28b4f91a583b86e49d183ed5d8
Richard Bramley <rabramley@gmail.com>
2019-06-24 10:05:07 +0100

    Missing onyx_external column

--- reporter/uhl_reports/briccs/data_quality/recruits_not_in_ciivcrm.py
+++ reporter/uhl_reports/briccs/data_quality/recruits_not_in_ciivcrm.py
@@ -14,41 +14,48 @@ class BriccsNotInCivicrm(SqlReport):
 
             sql='''
 
-SELECT  a.*
-FROM i2b2_app03_b1_Data.dbo.Load_Fully_Consented a
-WHERE NOT EXISTS (
-    SELECT 1
-    FROM i2b2_app03_b1_Data.dbo.LOAD_Civicrm b
-    WHERE a.studynumber = b.studynumber
-        AND (
-            b.is_recruited = 1 OR
-            b.is_excluded = 1 OR
-            b.is_withdrawn = 1 OR
-            b.is_duplicate = 1)
-    )
-    AND onyx_external = 0
-    AND redcap_external_consent = 0
+SELECT	StudyNumber, consent_date, 'REDCap UHL' AS source
+FROM	i2b2_app03_b1_Data.dbo.LOAD_Redcap
+WHERE BlankStudyNumber = 0
+	AND StudyNumber NOT IN (
+		SELECT StudyNumber
+		FROM i2b2_app03_b1_Data.dbo.LOAD_Civicrm b
+		WHERE (
+				b.is_recruited = 1 OR
+				b.is_excluded = 1 OR
+				b.is_withdrawn = 1 OR
+				b.is_duplicate = 1)
+			AND StudyNumber IS NOT NULL
+	)
+
+UNION
+
+SELECT	StudyNumber, consent_date, 'Onyx' AS source
+FROM	i2b2_app03_b1_Data.dbo.LOAD_Onyx
+WHERE BlankStudyNumber = 0
+	AND RecruitingSite = 'briccs_glenfield_recruitment'
+	AND StudyNumber NOT IN (
+		SELECT StudyNumber
+		FROM i2b2_app03_b1_Data.dbo.LOAD_Civicrm b
+		WHERE (
+				b.is_recruited = 1 OR
+				b.is_excluded = 1 OR
+				b.is_withdrawn = 1 OR
+				b.is_duplicate = 1)
+			AND StudyNumber IS NOT NULL
+	)
 
                 '''
         )
 
     def get_report_line(self, row):
         consent_date = (
-            '; Consent Date: {}'.format(row['consent_date_value'])
-            if row['consent_date_value'] else '')
+            '; Consent Date: {}'.format(row['consent_date'])
+            if row['consent_date'] else '')
 
         consent_source = ''
 
-        if row['civi_consent'] == 1:
-            consent_source += '; consent in civicrm'
-        if row['onyx_consent'] == 1:
-            consent_source += '; consent in onyx'
-        if row['redcap_consent'] == 1:
-            consent_source += '; consent in redcap'
-        if row['redcap_external_consent'] == 1:
-            consent_source += '; consent in redcap external'
-
-        return '- {}{}{}\r\n'.format(
+        return '- {}{}; {}\r\n'.format(
             row['StudyNumber'],
             consent_date,
-            consent_source)
+            row['source'])
