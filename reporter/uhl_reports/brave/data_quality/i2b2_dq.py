3616f986ec49f0bdd7abc773c6f16ad97d43d214
Richard Bramley <rabramley@gmail.com>
2020-04-03 14:03:24 +0100

    Unschedule i2b2 missing data for brave external

--- reporter/uhl_reports/brave/data_quality/i2b2_dq.py
+++ reporter/uhl_reports/brave/data_quality/i2b2_dq.py
@@ -12,6 +12,7 @@ from reporter.uhl_reports.i2b2.valid_enrolment_tests import (
 from reporter.emailing import (
     RECIPIENT_BRAVE_ADMIN as RECIPIENT_ADMIN,
 )
+from reporter.core import Schedule
 
 
 I2B2_DB = "i2b2_app03_brave_Data"
@@ -20,37 +21,37 @@ I2B2_DB = "i2b2_app03_brave_Data"
 class BravePatientSummaryDuplicatesReport(
         PatientSummaryDuplicatesReport):
     def __init__(self):
-        super().__init__(I2B2_DB)
+        super().__init__(database=I2B2_DB)
 
 
+# Cannot differentiate external participants who would not have these fields
 class BravePatientSummaryMissingData(
         PatientSummaryMissingData):
     def __init__(self):
         super().__init__(
-            I2B2_DB,
-            [
+            database=I2B2_DB,
+            fields=[
                 'CiviCrmId',
                 'NhsNumber',
                 'UhlSystemNumber',
                 'StudyNumber',
                 'Sex',
                 'DateOfBirth',
-            ]
-        )
+            ],schedule=Schedule.never,        )
 
 
 class BravePatientSummaryMissingParticipants(
         PatientSummaryMissingParticipants):
     def __init__(self):
-        super().__init__(I2B2_DB)
+        super().__init__(database=I2B2_DB)
 
 
 class BraveValidEnrolmentsStudyIdDuplicates(
         ValidEnrolmentsStudyIdDuplicates):
     def __init__(self):
         super().__init__(
-            I2B2_DB,
-            [RECIPIENT_ADMIN]
+            database=I2B2_DB,
+            recipients=[RECIPIENT_ADMIN]
         )
 
 
@@ -58,6 +59,6 @@ class BraveValidEnrolmentsContactMultipleRecruitments(
         ValidEnrolmentsContactMultipleRecruitments):
     def __init__(self):
         super().__init__(
-            I2B2_DB,
-            [RECIPIENT_ADMIN]
+            database=I2B2_DB,
+            recipients=[RECIPIENT_ADMIN],
         )
